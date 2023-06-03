75ee2d51bd87f276cc53be11253cf58b99de275f
Godofdrakes <GodofDrakes@gmail.com>
2023-06-03 16:07:25 -0700

    Test improvements

--- src/test/java/com/barrowspotentialtest/RewardPlannerTest.java
+++ src/test/java/com/barrowspotentialtest/RewardPlannerTest.java
@@ -5,202 +5,209 @@ import com.barrowspotential.RewardPlan;
 import com.barrowspotential.RewardPlanner;
 import com.barrowspotential.RewardTarget;
 import junit.framework.TestCase;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class RewardPlannerTest extends TestCase
 {
-    private static RewardPlan AllBrothers()
-    {
-        Map<Monster,Integer> map = new HashMap<>();
-        map.put( Monster.Ahrim, 1 );
-        map.put( Monster.Dharok, 1 );
-        map.put( Monster.Guthan, 1 );
-        map.put( Monster.Karil, 1 );
-        map.put( Monster.Torag, 1 );
-        map.put( Monster.Verac, 1 );
-        return new RewardPlan( map );
-    }
+	private final Logger logger = LoggerFactory.getLogger( RewardPlannerTest.class );
 
-    private static RewardPlan AllBrothersOptimalBloodRune()
-    {
-        Map<Monster,Integer> map = new HashMap<>();
-        map.put( Monster.Ahrim, 1 );
-        map.put( Monster.Dharok, 1 );
-        map.put( Monster.Guthan, 1 );
-        map.put( Monster.Karil, 1 );
-        map.put( Monster.Torag, 1 );
-        map.put( Monster.Verac, 1 );
+	private static RewardPlan AllBrothers()
+	{
+		Map<Monster,Integer> map = new HashMap<>();
+		map.put( Monster.Ahrim, 1 );
+		map.put( Monster.Dharok, 1 );
+		map.put( Monster.Guthan, 1 );
+		map.put( Monster.Karil, 1 );
+		map.put( Monster.Torag, 1 );
+		map.put( Monster.Verac, 1 );
+		return new RewardPlan( map );
+	}
 
-        // The optimal plan is 1 giant crypt spider, 1 skeleton, and 1 crypt spider (880 points)
-        // 3 bloodworms and 1 crypt spider is also 880 points but is more steps, and therefor not optimal
-        map.put( Monster.GiantCryptSpider, 1 );
-        map.put( Monster.Skeleton, 1 );
-        map.put( Monster.CryptSpider, 1 );
-        return new RewardPlan( map );
-    }
+	private static RewardPlan AllBrothersOptimalBloodRune()
+	{
+		Map<Monster,Integer> map = new HashMap<>();
+		map.put( Monster.Ahrim, 1 );
+		map.put( Monster.Dharok, 1 );
+		map.put( Monster.Guthan, 1 );
+		map.put( Monster.Karil, 1 );
+		map.put( Monster.Torag, 1 );
+		map.put( Monster.Verac, 1 );
 
-    private static void LogPlan( RewardPlan plan )
-    {
-        assert ( plan != null );
+		// The optimal plan is 1 giant crypt spider, 1 skeleton, and 1 crypt spider (880 points)
+		// 3 bloodworms and 1 crypt spider is also 880 points but is more steps, and therefor not optimal
+		map.put( Monster.GiantCryptSpider, 1 );
+		map.put( Monster.Skeleton, 1 );
+		map.put( Monster.CryptSpider, 1 );
+		return new RewardPlan( map );
+	}
 
-        int steps = 0;
-        int score = 0;
+	private static void LogPlan( Logger logger, RewardPlan plan )
+	{
+		assert ( plan != null );
 
-        for ( Map.Entry<Monster,Integer> entry : plan.monsters.entrySet() )
-        {
-            int count = entry.getValue();
-            int value = entry.getKey().getCombatLevel() * count;
-            String displayName = entry.getKey().getDisplayName();
-            System.out.printf( "x%d %s (%d)\n", count, displayName, value );
+		int steps = 0;
+		int score = 0;
 
-            steps += count;
-            score += value;
-        }
+		for ( Map.Entry<Monster,Integer> entry : plan.monsters.entrySet() )
+		{
+			int count = entry.getValue();
+			int value = entry.getKey().getCombatLevel() * count;
+			String displayName = entry.getKey().getDisplayName();
+			logger.info( "x{} {} ({})", count, displayName, value );
 
-        System.out.printf( "Steps: %d\n", steps );
-        System.out.printf( "Score: %d\n", score );
-    }
+			steps += count;
+			score += value;
+		}
 
-    private static RewardPlan RunPlanner( RewardPlanner planner, int maxIterations )
-    {
-        int iteration = 0;
+		logger.info( "Steps: {}", steps );
+		logger.info( "Score: {}", score );
+	}
 
-        RewardPlan plan = null;
+	private static RewardPlan RunPlanner( Logger logger, RewardPlanner planner, int maxIterations )
+	{
+		int iteration = 0;
 
-        for ( ; iteration < maxIterations; ++iteration )
-        {
-            plan = planner.search();
-            if ( plan != null )
-                break;
-        }
+		RewardPlan plan = null;
 
-        System.out.printf( "Ran %d/%d iterations\n", iteration, maxIterations );
+		for ( ; iteration < maxIterations; ++iteration )
+		{
+			plan = planner.search();
+			if ( plan != null )
+				break;
+		}
 
-        return plan;
-    }
+		logger.info( "Ran {}/{} iterations", iteration, maxIterations );
 
-    // assert that for the given base plan there is an optimal plan to reach the target
-    private RewardPlan assertOptimalPlan( RewardTarget rewardTarget, RewardPlan basePlan )
-    {
-        System.out.println( "searching for optimal plan" );
+		return plan;
+	}
 
-        RewardPlanner planner = new RewardPlanner();
+	// assert that for the given base plan there is an optimal plan to reach the target
+	private static RewardPlan assertOptimalPlan( Logger logger, RewardTarget rewardTarget, RewardPlan basePlan )
+	{
+		logger.info( "searching for optimal plan" );
 
-        planner.mode = rewardTarget.getMaxValue() < Integer.MAX_VALUE
-            ? RewardPlanner.Mode.NEAREST
-            : RewardPlanner.Mode.ANY;
-        planner.reset( basePlan, rewardTarget.getMaxValue() < Integer.MAX_VALUE
-            ? rewardTarget.getMaxValue()
-            : rewardTarget.getMinValue() );
+		RewardPlanner planner = new RewardPlanner();
 
-        // allow a lot of iterations here since this isn't runtime code
-        RewardPlan plan = RunPlanner( planner, 1000 );
+		planner.mode = rewardTarget.getMaxValue() < Integer.MAX_VALUE
+			? RewardPlanner.Mode.NEAREST
+			: RewardPlanner.Mode.ANY;
+		planner.reset( basePlan, rewardTarget.getMaxValue() < Integer.MAX_VALUE
+			? rewardTarget.getMaxValue()
+			: rewardTarget.getMinValue() );
 
-        // do not accept partial solution
+		// allow a lot of iterations here since this isn't runtime code
+		RewardPlan plan = RunPlanner( logger, planner, 1000 );
 
-        assertNotNull( plan );
+		// do not accept partial solution
 
-        LogPlan( plan );
+		assertNotNull( plan );
 
-        assertEquals( rewardTarget.getMaxValue(), plan.GetRewardPotential() );
+		LogPlan( logger, plan );
 
-        System.out.println();
-        return plan;
-    }
+		assertEquals( rewardTarget.getMaxValue(), plan.GetRewardPotential() );
 
-    // assert that for the given base plan there is a valid plan to reach the target within 20 iterations
-    private RewardPlan assertValidPlan( RewardTarget rewardTarget, RewardPlan basePlan )
-    {
-        System.out.println( "searching for valid plan" );
+		return plan;
+	}
 
-        RewardPlanner planner = new RewardPlanner();
+	// assert that for the given base plan there is a valid plan to reach the target within 20 iterations
+	private static RewardPlan assertValidPlan( Logger logger, RewardTarget rewardTarget, RewardPlan basePlan )
+	{
+		logger.info( "searching for valid plan" );
 
-        planner.mode = rewardTarget.getMaxValue() < Integer.MAX_VALUE
-            ? RewardPlanner.Mode.NEAREST
-            : RewardPlanner.Mode.ANY;
-        planner.reset( basePlan, rewardTarget.getMaxValue() < Integer.MAX_VALUE
-            ? rewardTarget.getMaxValue()
-            : rewardTarget.getMinValue() );
+		RewardPlanner planner = new RewardPlanner();
 
-        // at runtime the number of iterations is limited, test that here
-        RewardPlan plan = RunPlanner( planner, 20 );
+		planner.mode = rewardTarget.getMaxValue() < Integer.MAX_VALUE
+			? RewardPlanner.Mode.NEAREST
+			: RewardPlanner.Mode.ANY;
+		planner.reset( basePlan, rewardTarget.getMaxValue() < Integer.MAX_VALUE
+			? rewardTarget.getMaxValue()
+			: rewardTarget.getMinValue() );
 
-        if ( plan == null )
-        {
-            plan = planner.takeBest();
-        }
+		// at runtime the number of iterations is limited, test that here
+		RewardPlan plan = RunPlanner( logger, planner, 20 );
 
-        assertNotNull( plan );
+		if ( plan == null )
+		{
+			plan = planner.takeBest();
+		}
 
-        LogPlan( plan );
+		assertNotNull( plan );
 
-        assertTrue( "planned potential < minimum value", plan.GetRewardPotential() >= rewardTarget.getMinValue() );
-        assertTrue( "planned potential > maximum value value",
-            plan.GetRewardPotential() <= rewardTarget.getMaxValue() );
+		LogPlan( logger, plan );
 
-        System.out.println();
-        return plan;
-    }
+		assertTrue( "planned potential < minimum value",
+			plan.GetRewardPotential() >= rewardTarget.getMinValue() );
+		assertTrue( "planned potential > maximum value value",
+			plan.GetRewardPotential() <= rewardTarget.getMaxValue() );
 
-    public void testBaseIsBest()
-    {
-        RewardPlan basePlan = RewardPlan.Default;
+		return plan;
+	}
 
-        RewardPlanner planner = new RewardPlanner();
+	public void testBaseIsBest()
+	{
+		RewardPlan basePlan = RewardPlan.Default;
 
-        planner.reset( basePlan, RewardTarget.Coins.getMaxValue() );
+		RewardPlanner planner = new RewardPlanner();
 
-        // At minimum the best plan should be the base plan
-        assertEquals( basePlan, planner.takeBest() );
-    }
+		planner.reset( basePlan, RewardTarget.Coins.getMaxValue() );
 
-    public void testOptimalKillAll()
-    {
-        RewardPlan allBrothers = AllBrothers();
-        RewardPlan plan = assertOptimalPlan( RewardTarget.BloodRune, allBrothers );
-        assertEquals( "plan does not match known optimal plan", AllBrothersOptimalBloodRune(), plan );
+		// At minimum the best plan should be the base plan
+		assertEquals( basePlan, planner.takeBest() );
+	}
 
-        assertValidPlan( RewardTarget.DragonMed, allBrothers );
-    }
+	public void testOptimalKillAll()
+	{
+		RewardPlan allBrothers = AllBrothers();
+		RewardPlan plan = assertOptimalPlan( logger, RewardTarget.BloodRune, allBrothers );
 
-    public void testOptimalKillSingle()
-    {
-        RewardPlan combatLevel98 = RewardPlan.Create( Monster.Ahrim );
-        RewardPlan combatLevel115 = RewardPlan.Create( Monster.Dharok );
-        assertOptimalPlan( RewardTarget.BloodRune, combatLevel98 );
-        assertOptimalPlan( RewardTarget.BloodRune, combatLevel115 );
-        assertValidPlan( RewardTarget.BloodRune, combatLevel98 );
-        assertValidPlan( RewardTarget.BloodRune, combatLevel115 );
-        assertValidPlan( RewardTarget.DragonMed, combatLevel98 );
-        assertValidPlan( RewardTarget.DragonMed, combatLevel115 );
-    }
+		assertEquals( "plan does not match known optimal plan",
+			AllBrothersOptimalBloodRune(),
+			plan );
 
-    public void testOptimalKillPartial()
-    {
-        RewardPlan combatLevel98 = RewardPlan.Create(
-            //Monster.Ahrim, // combat level 98
-            Monster.Dharok,
-            Monster.Guthan,
-            Monster.Karil,
-            Monster.Torag,
-            Monster.Verac
-        );
-        RewardPlan combatLevel115 = RewardPlan.Create(
-            //Monster.Ahrim, // combat level 98
-            Monster.Dharok,
-            Monster.Guthan,
-            Monster.Karil,
-            Monster.Torag,
-            Monster.Verac
-        );
+		assertValidPlan( logger, RewardTarget.DragonMed, allBrothers );
+	}
 
-        assertOptimalPlan( RewardTarget.BloodRune, combatLevel98 );
-        assertOptimalPlan( RewardTarget.BloodRune, combatLevel115 );
-        assertValidPlan( RewardTarget.BloodRune, combatLevel98 );
-        assertValidPlan( RewardTarget.BloodRune, combatLevel115 );
-        assertValidPlan( RewardTarget.DragonMed, combatLevel98 );
-        assertValidPlan( RewardTarget.DragonMed, combatLevel115 );
-    }
+	public void testOptimalKillSingle()
+	{
+		RewardPlan combatLevel98 = RewardPlan.Create( Monster.Ahrim );
+		RewardPlan combatLevel115 = RewardPlan.Create( Monster.Dharok );
+
+		assertOptimalPlan( logger, RewardTarget.BloodRune, combatLevel98 );
+		assertOptimalPlan( logger, RewardTarget.BloodRune, combatLevel115 );
+		assertValidPlan( logger, RewardTarget.BloodRune, combatLevel98 );
+		assertValidPlan( logger, RewardTarget.BloodRune, combatLevel115 );
+		assertValidPlan( logger, RewardTarget.DragonMed, combatLevel98 );
+		assertValidPlan( logger, RewardTarget.DragonMed, combatLevel115 );
+	}
+
+	public void testOptimalKillPartial()
+	{
+		RewardPlan combatLevel98 = RewardPlan.Create(
+			//Monster.Ahrim, // combat level 98
+			Monster.Dharok,
+			Monster.Guthan,
+			Monster.Karil,
+			Monster.Torag,
+			Monster.Verac
+		);
+		RewardPlan combatLevel115 = RewardPlan.Create(
+			Monster.Ahrim,
+			//Monster.Dharok, // combat level 115
+			Monster.Guthan,
+			Monster.Karil,
+			Monster.Torag,
+			Monster.Verac
+		);
+
+		assertOptimalPlan( logger, RewardTarget.BloodRune, combatLevel98 );
+		assertOptimalPlan( logger, RewardTarget.BloodRune, combatLevel115 );
+		assertValidPlan( logger, RewardTarget.BloodRune, combatLevel98 );
+		assertValidPlan( logger, RewardTarget.BloodRune, combatLevel115 );
+		assertValidPlan( logger, RewardTarget.DragonMed, combatLevel98 );
+		assertValidPlan( logger, RewardTarget.DragonMed, combatLevel115 );
+	}
 }
